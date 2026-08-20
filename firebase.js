c25cb069f339f8b927d6a9823a5042be368f2c1e
renee9211 <reneew9211@gmail.com>
2026-08-20 10:20:50 +0800

    Add Firebase authentication and Firestore configuration

---
+++ firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore, collection, doc, getDocs, getDoc, setDoc, addDoc, updateDoc, deleteDoc, serverTimestamp, query, orderBy } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeK_P3N8jECNSuEq1VmXkinY54r7RJTkk",
+  authDomain: "event-dashboard-a3773.firebaseapp.com",
+  projectId: "event-dashboard-a3773",
+  storageBucket: "event-dashboard-a3773.firebasestorage.app",
+  messagingSenderId: "531470257400",
+  appId: "1:531470257400:web:eb3b364cd884591f4fff0d",
+  measurementId: "G-DJKJ2KC4NX"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export {
+  auth, db, provider,
+  signInWithPopup, signOut, onAuthStateChanged,
+  collection, doc, getDocs, getDoc, setDoc, addDoc, updateDoc, deleteDoc,
+  serverTimestamp, query, orderBy
+};
